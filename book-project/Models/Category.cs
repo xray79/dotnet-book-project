52d49db050ba5ed59dd014a089c9dff271df87e6
Ibnul Huq <ibnul.huq@gmail.com>
2023-12-23 10:33:11 +0000

    Add create category form

--- book-project/Models/Category.cs
+++ book-project/Models/Category.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace book_project.Models;
@@ -7,6 +8,9 @@ public class Category
     [Key]
     public int Id { get; set; }
     [Required]
+    [MaxLength(30)]
     public string? Name { get; set; }
+    [DisplayName("Display Order")]
+    [Range(1,100)]
     public int DisplayOrder { get; set; }
 }
