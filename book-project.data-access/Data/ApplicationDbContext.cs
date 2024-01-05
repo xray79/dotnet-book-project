f9d7e4c0c366e8988e6abecc930bcddebbd6057e
Ibnul Huq <ibnul.huq@gmail.com>
2024-01-05 20:04:23 +0000

    Add authorization and Authentication using Identity

--- book-project.data-access/Data/ApplicationDbContext.cs
+++ book-project.data-access/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 using book_project.models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace book_project.data_access.Data;
 
-public class ApplicationDbContext: DbContext
+public class ApplicationDbContext: IdentityDbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
     {
@@ -11,9 +12,12 @@ public class ApplicationDbContext: DbContext
 
     public DbSet<Category> Categories { get; set; }
     public DbSet<Product> Products { get; set; }
+    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
+        
         modelBuilder.Entity<Category>().HasData(
             new Category { Id = 1, DisplayOrder = 1, Name = "Fiction" },
             new Category { Id = 2, DisplayOrder = 2, Name = "Non-Fiction" }
--- book-project/Areas/Admin/Controllers/ProductController.cs
+++ book-project/Areas/Admin/Controllers/ProductController.cs
@@ -1,12 +1,15 @@
 using book_project.data_access.Repository.IRepository;
 using book_project.models;
 using book_project.models.ViewModels;
+using book_project.utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace book_project.Areas.Admin.Controllers;
 
 [Area("Admin")]
+[Authorize(Roles = SD.Role_Admin)]
 public class ProductController : Controller
 {
     private readonly IUnitOfWork _unitOfWork;
